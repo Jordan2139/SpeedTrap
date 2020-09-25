04aef7139d49c58e87b43fe7c3b5bcb29d2509b1
Jordan.#2139 <65438497+Jordan2139@users.noreply.github.com>
2020-09-25 18:33:15 -0400

    Rewrote a bit to work with the version checker

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+------------------------------------------------------------
+-- SpeedTrap- A Simple FiveM Script, Made By Jordan.#2139 --
+------------------------------------------------------------
+
+fx_version "bodacious"
+game "gta5"
+
+-- Define the resource metadata
+name "SpeedTrap"
+description "A simple failrp speed monitor"
+author "Jordan.#2139"
+version "1.0.0"
+
+
+client_script "config.lua"
+client_script "client.lua"
+
+server_script "config.lua"
+server_script "server.lua"
+server_script "version_check.lua"
